39ea7daa97e5aac681760a2d342b95760bdad8dd
Johannes Filter <hi@jfilter.de>
2020-09-01 18:22:18 +0200

    only download new data if needed

--- serve/add_secret.py
+++ serve/add_secret.py
@@ -1,6 +1,33 @@
 from pathlib import Path
 
+import dataset
+import requests
+
 urls = Path('scrapers.txt').read_text().split()
-key = '/data.sqlite?key=' + Path('secrets.txt').read_text().split()[0]
-urls = [x + key for x in urls]
-Path('input.txt').write_text('\n'.join(urls))
+key = Path('secrets.txt').read_text().split()[0]
+key_url = '/data.sqlite?key=' + key
+
+to_download = []
+
+# only fetch new data if needed
+
+for x in urls:
+    morph_api_url = x.replace('morph.io', 'api.morph.io') + '/data.json'
+    print(morph_api_url)
+    r = requests.get(morph_api_url, params={
+    'key': key,
+    'query': 'select count(*) from "incidents"'
+    })
+
+    r.raise_for_status()
+
+    db_name = x.split('/')[-1] + '.db'
+    db = dataset.connect(f'sqlite:///data/{db_name}')
+    db_count = db['incidents'].count()
+
+    morph_count = r.json()[0]['count(*)']
+    print(db_count, morph_count)
+    if db_count != morph_count:
+        to_download.append(x + key_url + ' ' + db_name)
+
+Path('input.txt').write_text('\n'.join(to_download) + '\n')
--- serve/update.sh
+++ serve/update.sh
@@ -5,17 +5,15 @@ set -x
 # pass random argument (bash serve.sh --random) to avoid updating data
 if [ -z "$1" ]; then
     echo "Fetching new data"
-    mkdir -p data && rm -f data/* && rm -f input.txt && python3 add_secret.py
-    i=0
-    wget -i input.txt -P data
-    for fn in data/*; do
-        mv $fn data/data_$i.db
-        i=$((i + 1))
-    done && rm input.txt
+    mkdir -p data && rm -f input.txt && python3 add_secret.py
+
+    # NB: file has to be ended with `\n`
+    while IFS=' ' read -r url file_name; do
+        wget -O data/"$file_name" "$url"
+    done <input.txt
+    rm input.txt
 fi
 
-# install dependencies, wrangle data, serve
-# pip3 install datasette datasette-cluster-map clean-text dataset requests notebook &&
 rm -f rechtegewalt.db*
 jupyter nbconvert --execute combine.ipynb --ExecutePreprocessor.timeout=999999999999999999
 python add_geo.py
