8055f7af3644cfa409d9e8e8ffe73f48ee6d1030
Johannes Filter <hi@jfilter.de>
2021-02-07 02:19:17 +0100

    fix serve files

--- aggregate/serve.sh
+++ aggregate/serve.sh
@@ -5,4 +5,4 @@
 cd /home/user/data.rechtegewalt.info &&
 /home/user/.poetry/bin/poetry run datasette serve --config max_returned_rows:1000000 \
 --load-extension=/usr/lib/x86_64-linux-gnu/mod_spatialite.so \
---cors -h 0.0.0.0 /home/user/data.rechtegewalt.info/serve/rechtegewalt.db >> /home/user/serve.log
+--cors -h 0.0.0.0 /home/user/data.rechtegewalt.info/aggregate/rechtegewalt.db >> /home/user/serve.log
---
+++ aggregate/serve_dokku.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+poetry run datasette serve --config max_returned_rows:1000000 \
+--load-extension=/usr/lib/x86_64-linux-gnu/mod_spatialite.so \
+--cors -h 0.0.0.0 rechtegewalt.db
